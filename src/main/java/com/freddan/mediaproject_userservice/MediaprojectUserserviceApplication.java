19acc713d2f46bf3b97aa8c517563aa4b153cf30
Fredrik Lundell <fredrik_lundell@icloud.com>
2024-10-05 15:52:38 +0200

    userservice

---
+++ src/main/java/com/freddan/mediaproject_userservice/MediaprojectUserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.freddan.mediaproject_userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaprojectUserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaprojectUserserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freddan/mediaproject_userservice/MediaprojectUserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freddan.mediaproject_userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaprojectUserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
